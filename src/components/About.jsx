9b30ba80df9e5fa34c7e117e1a2cb9e1dc4af310
sumitcoder007 <sumityadav0370@gmail.com>
2024-09-20 20:47:34 +0530

    Done !!

---
+++ src/components/About.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+
+function About() {
+  return (
+    <div
+      name="About"
+      className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-20"
+    >
+      <div>
+        <h1 className="text-3xl font-bold mb-5">About</h1>
+        <p>
+        I am Sumit Yadav, a highly dedicated and passionate Full Stack Web Developer with a strong specialization in the MERN stack. My expertise spans across Next.js, React.js, JavaScript, HTML, CSS, MongoDB, Express.js, and Node.js, enabling me to build dynamic, scalable, and robust web applications.
+        </p>
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          SKILLS AND EXPERTISE
+        </h1>
+        <span>
+        Libraries and Tools: Express.js, Tailwind CSS
+
+ JavaScript, React.js,Tailwind CSS, Bootstrap
+Node.js, Express.js, MongoDB, Mongoose
+Control Git, GitHub
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          EXPERIANCE
+        </h1>
+        <span>
+        • Mern Stack Developer - Afame Technologies
+        <br />
+• Responsibilities and maintained web applications
+Ensured cross-browser compatibility and responsiveness. Collaborated with the design team for user-friendly
+interfaces. Utilized HTML, CSS, JavaScript, and React.js. Optimized web applications for speed and scalability
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          ACHIEVEMENTS AND AWARDS
+        </h1>
+        <span>
+• Completed Full-Stack Web Development Course: Gained hands-on experience in building full-stack applications
+using the MERN stack.
+<br />
+• Active participation on LeetCode, having solved over 200+ problems
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+        EXTRACURRICULAR ACTIVITIES
+        </h1>
+        <p>
+        • Leetcode Daily Problems • Consistently solved LeetCode problems to enhance problem-solving skills and
+algorithmic understanding.
+<br />
+• MERN Stack Developer • Contribute to open-source MERN projects and participate in hackathons.
+• Attend tech meetups and conferences focused on MERN technologies.
+• Develop personal projects using the MERN stack
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
